2ceb54cc06af2d08601b75c606fbd4db088bee00
vennilashanthini <132681721+vennilashanthini@users.noreply.github.com>
2024-03-18 12:14:53 +0530

    ResumeTasks

---
+++ Resume/script.js
@@ -0,0 +1,43 @@
+const myResume = [
+  
+    { "Name":"Vennilashanthini Murugan",
+ "Contact":7358550747, 
+ "email id":"mshanthini95@gmail.com"},
+
+ { "Education":"KARPAGA VINYAGA COLLEGE",
+ "Degree":"BACHELOR IN TECHNOLOGY", 
+ "Period":"Aug 2013 -May 2017",
+"CGPA": 7.8},
+
+{ "Experience":"AMAZON DEVELOPMENT CENTRE",
+"Period":"Oct2021 - Nov2023",
+"Designation":"Automation Analyst"
+},
+{
+    "Responsibiles":"Worked on project NON IDENTICAL which were allocated on weekly basis to find why IDENTICAL were moved as NON IDENTICAL and ensure the target was achieved within the given time.As part of project NON IDENTICAL Maintenance of Competitor made sure the configuration was optimized enough to deliver the desired output. Identified a potential gap in configuration which led to 100% reduction NON IDENTICAL contribution across regions.Raised this issue to other Product line teams and performed reconfiguration. Due to this early identification of this issue we were able to maintain our important team metrics for the week.Coordinated with Operation team on identifying crawl error and scrape error inharlogs.Received many accolodes as Automation Analyst for Proactive fixing of glitches"
+},
+{ "Experience":"AMAZON DEVELOPMENT CENTRE",
+"Period":"Jul2019 - Sep2021",
+"Designation":"Retail Associate"
+},
+{
+    "Responsibiles":"Awarded for exceeding expectations and showing great passion towards work.Volunteered to take on additional responsibilities for the team, displayed curiosity to learn broader aspects of the process and contributed to process improvement ideas to improve experience for the customers."
+},
+{ "Experience":"HDFC BANK",
+"Period":"Dec2017 - Sep2018",
+"Designation":"Telecaller"
+},
+{
+    "Responsibilites":"Handled Customer queries for Payment Process of Credit Card."
+},
+{ "AWards":"Awarded Rookie Rockstar award in R&R for delivering strong results against the assigned goals, Being Proactive, demonstrated ownership and volunteered to take on additional responsibilities for the team in the month of Oct-2023. Zenith award was awarded to the team recognizing exceptional results for business and customers and to have raised the bar against the project goals through innovation, change and timely execution in the month of Feb-2023."
+},
+{
+    "Skills":"Xpath,Regex,HTML,SQL,Java"
+},
+{
+    "CERTIFICATION":"Automation Testing with Java and selenium,Dot Net Technology"
+}
+]
+
+console.log(myResume);
